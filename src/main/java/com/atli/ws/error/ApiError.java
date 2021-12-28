61ddcd401800217065c24de80c727ed76dd60840
nuatli <nuatli@havelsan.com.tr>
2021-12-28 15:12:57 +0300

    24

---
+++ src/main/java/com/atli/ws/error/ApiError.java
@@ -0,0 +1,52 @@
+package com.atli.ws.error;
+
+import java.util.Date;
+import java.util.Map;
+import lombok.Data;
+
+@Data
+public class ApiError {
+	private int status;
+	private String message;
+	private String path;
+	private long timestamp = new Date().getTime();
+	private Map<String,String> validationErrors;
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Map<String, String> getValidationErrors() {
+		return validationErrors;
+	}
+	public void setValidationErrors(Map<String, String> validationErrors) {
+		this.validationErrors = validationErrors;
+	}
+	
+	public ApiError(int Status,String Message,String Path){
+		this.status = Status;
+		this.message= Message;
+		this.path = Path;
+	}
+	
+	
+}
--- src/main/java/com/atli/ws/user/UserController.java
+++ src/main/java/com/atli/ws/user/UserController.java
@@ -1,31 +1,58 @@
 package com.atli.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atli.ws.error.ApiError;
 import com.atli.ws.shared.GenericResponse;
 
 @RestController
 public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
-	/*
-	@Autowired
-	UserService userService; 
-	*/
 	@Autowired
 	UserService userService;
 	
+	/*
 	@PostMapping("/api/0.0.1/users")
 	public GenericResponse createUser(@RequestBody User user) {
 		log.info(user.toString());
-		//System.out.println(userRepository.toString());
 		userService.save(user);
 		return new GenericResponse("User Created");
 	}
+	*/
+	@PostMapping("/api/0.0.1/users")
+	public ResponseEntity<?> createUser(@RequestBody User user) {
+		ApiError error = new ApiError(400, "Validation Error", "/api/0.0.1/users");
+		Map<String,String> validationErrors = new HashMap<>();
+		String username = user.getUsername();
+		String displayName = user.getDisplayName();
+		
+		if(username == null || username.isEmpty()){
+			validationErrors.put("username", "Username Cannot Be Empty");
+		}
+		
+		if(displayName == null || displayName.isEmpty()){
+			validationErrors.put("displayName", "Display Name Cannot Be Empty");
 
+		}
+		if(validationErrors.size() > 0){
+			error.setValidationErrors(validationErrors);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		}
+		
+		
+		userService.save(user);
+		return new ResponseEntity(new GenericResponse("User Created"), HttpStatus.CREATED);
+	}
 }
